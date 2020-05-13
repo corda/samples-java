cd3f5e1a4eaaf2f95fe6d7b0c6cf2232eaafeee0
peterli-r3 <51169685+peterli-r3@users.noreply.github.com>
2020-05-13 19:46:43 -0400

    supplychain-account

---
+++ Accounts/supplychain/workflows/src/test/java/com/supplychain/FlowTests.java
@@ -0,0 +1,37 @@
+package com.supplychain;
+
+import com.google.common.collect.ImmutableList;
+import net.corda.testing.node.MockNetwork;
+import net.corda.testing.node.MockNetworkParameters;
+import net.corda.testing.node.StartedMockNode;
+import net.corda.testing.node.TestCordapp;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FlowTests {
+    private final MockNetwork network = new MockNetwork(new MockNetworkParameters(ImmutableList.of(
+        TestCordapp.findCordapp("com.supplychain.contracts"),
+        TestCordapp.findCordapp("com.supplychain.flows")
+    )));
+    private final StartedMockNode a = network.createNode();
+    private final StartedMockNode b = network.createNode();
+
+    public FlowTests() {
+    }
+
+    @Before
+    public void setup() {
+        network.runNetwork();
+    }
+
+    @After
+    public void tearDown() {
+        network.stopNodes();
+    }
+
+    @Test
+    public void dummyTest() {
+
+    }
+}
