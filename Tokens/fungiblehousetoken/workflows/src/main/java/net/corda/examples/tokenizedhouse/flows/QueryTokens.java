eca5f2dc2477a14ec94a1e6c55b339b8e4edb7d7
Quan-Wenjiang <949139200@qq.com>
2020-10-29 10:01:28 +0800

    Update GetTokenBalance

--- Tokens/fungiblehousetoken/workflows/src/main/java/net/corda/examples/tokenizedhouse/flows/QueryTokens.java
+++ Tokens/fungiblehousetoken/workflows/src/main/java/net/corda/examples/tokenizedhouse/flows/QueryTokens.java
@@ -4,6 +4,7 @@ import co.paralleluniverse.fibers.Suspendable;
 import com.r3.corda.lib.tokens.contracts.types.TokenPointer;
 import com.r3.corda.lib.tokens.contracts.types.TokenType;
 import com.r3.corda.lib.tokens.workflows.utilities.QueryUtilities;
+import net.corda.core.contracts.TransactionState;
 import net.corda.examples.tokenizedhouse.states.FungibleHouseTokenState;
 import net.corda.core.contracts.Amount;
 import net.corda.core.contracts.StateAndRef;
@@ -12,6 +13,9 @@ import net.corda.core.flows.FlowLogic;
 import net.corda.core.flows.InitiatingFlow;
 import net.corda.core.flows.StartableByRPC;
 import net.corda.core.utilities.ProgressTracker;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class QueryTokens {
 
@@ -34,20 +38,27 @@ public class QueryTokens {
         @Override
         @Suspendable
         public String call() throws FlowException {
-            //get house states on ledger with uuid as input tokenId
-            StateAndRef<FungibleHouseTokenState> stateAndRef = getServiceHub().getVaultService().
+            //get a set of the RealEstateEvolvableTokenType object on ledger with uuid as input tokenId
+            Set<FungibleHouseTokenState> evolvableTokenTypeSet = getServiceHub().getVaultService().
                     queryBy(FungibleHouseTokenState.class).getStates().stream()
-                    .filter(sf->sf.getState().getData().getSymbol().equals(symbol)).findAny()
-                    .orElseThrow(()-> new IllegalArgumentException("FungibleHouseTokenState symbol=\""+symbol+"\" not found from vault"));
+                    .filter(sf->sf.getState().getData().getSymbol().equals(symbol)).map(StateAndRef::getState)
+                    .map(TransactionState::getData).collect(Collectors.toSet());
+            if (evolvableTokenTypeSet.isEmpty()){
+                throw new IllegalArgumentException("FungibleHouseTokenState symbol=\""+symbol+"\" not found from vault");
+            }
 
-            //get the RealEstateEvolvableTokenType object
-            FungibleHouseTokenState evolvableTokenType = stateAndRef.getState().getData();
+            // Save the result
+            String result="";
 
-            //get the pointer pointer to the house
-            TokenPointer<FungibleHouseTokenState> tokenPointer = evolvableTokenType.toPointer(FungibleHouseTokenState.class);
+            for (FungibleHouseTokenState evolvableTokenType : evolvableTokenTypeSet){
+                //get the pointer pointer to the house
+                TokenPointer<FungibleHouseTokenState> tokenPointer = evolvableTokenType.toPointer(FungibleHouseTokenState.class);
+                //query balance or each different Token
+                Amount<TokenType> amount = QueryUtilities.tokenBalance(getServiceHub().getVaultService(), tokenPointer);
+                result += "\n You currently have "+ amount.getQuantity()+ " " + symbol + " Tokens issued by "+evolvableTokenType.getMaintainer()+"\n";
 
-            Amount<TokenType> amount = QueryUtilities.tokenBalance(getServiceHub().getVaultService(), tokenPointer);
-            return "\n You currently have "+ amount.getQuantity()+ " " +symbol + " Tokens\n";
+            }
+            return result;
         }
     }
 
