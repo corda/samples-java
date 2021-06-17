b5a9a74c8e0e48ee3c4f2aa2d39402d45659f2ba
Ollie Gilbey <oliver.gilbey@r3.com>
2021-06-17 16:10:46 +0100

    Copy Template into Accounts
    Initial commit to add all files from template to the Accounts sample directory

---
+++ Accounts/election-voting/workflows/src/test/java/com/template/FlowTests.java
@@ -0,0 +1,51 @@
+package com.template;
+
+import com.google.common.collect.ImmutableList;
+import com.template.flows.Initiator;
+import com.template.flows.Responder;
+import com.template.states.TemplateState;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.QueryCriteria;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.testing.node.MockNetwork;
+import net.corda.testing.node.MockNetworkParameters;
+import net.corda.testing.node.StartedMockNode;
+import net.corda.testing.node.TestCordapp;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.Future;
+
+public class FlowTests {
+    private MockNetwork network;
+    private StartedMockNode a;
+    private StartedMockNode b;
+
+    @Before
+    public void setup() {
+        network = new MockNetwork(new MockNetworkParameters().withCordappsForAllNodes(ImmutableList.of(
+                TestCordapp.findCordapp("com.template.contracts"),
+                TestCordapp.findCordapp("com.template.flows"))));
+        a = network.createPartyNode(null);
+        b = network.createPartyNode(null);
+        network.runNetwork();
+    }
+
+    @After
+    public void tearDown() {
+        network.stopNodes();
+    }
+
+    @Test
+    public void dummyTest() {
+        Initiator flow = new Initiator(b.getInfo().getLegalIdentities().get(0));
+        Future<SignedTransaction> future = a.startFlow(flow);
+        network.runNetwork();
+
+        //successful query means the state is stored at node b's vault. Flow went through.
+        QueryCriteria inputCriteria = new QueryCriteria.VaultQueryCriteria().withStatus(Vault.StateStatus.UNCONSUMED);
+        TemplateState state = b.getServices().getVaultService()
+                .queryBy(TemplateState.class,inputCriteria).getStates().get(0).getState().getData();
+    }
+}
