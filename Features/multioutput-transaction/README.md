549a91689b2c3ae60fab4fd785a1268fe428a9b0
peterli-r3 <51169685+peterli-r3@users.noreply.github.com>
2022-10-25 14:24:41 +0800

    add new sample cordapp

---
+++ Features/multioutput-transaction/README.md
@@ -0,0 +1,58 @@
+<p align="center">
+  <img src="https://www.corda.net/wp-content/uploads/2016/11/fg005_corda_b.png" alt="Corda" width="500">
+</p>
+
+# Multi-Output Transaction Cordapp
+
+In this cordapp, we will demo how to produce two outputs in one transaction. 
+It is a simple use case that we will have two states. One state is called 
+SubcountState and the other state is called OmniState, for which the OmniState keeps track of the accumulated balance of 
+the SubCountState amounts. Our goal is to show you how to produce two outputs in one transaction.
+
+# Pre-Requisites
+
+See https://docs.corda.net/getting-set-up.html.
+
+# Usage
+
+## Running the nodes
+
+Open a terminal and go to the project root directory and type: (to deploy the nodes using bootstrapper)
+```
+./gradlew clean deployNodes
+```
+Then type: (to run the nodes)
+```
+./build/nodes/runnodes
+```
+
+## Interacting with the nodes
+In PartyA's node, run the following commands to initiate a single output transaction to PartyB
+```
+flow start SendOneOutputFlowInitiator receiver: PartyB, amount: 10
+```
+Then we can go to PartyB's node to look at the expected output from the first transaction.
+```
+run vaultQuery contractStateType: net.corda.samples.multioutput.states.SubCountState
+```
+Now, we would like to bring in the second output. Now, back to PartyA's node, run the following command: 
+```
+flow start SendTwoOutputFlowInitiator receiver: PartyB, amount: 15
+```
+we can now go to PartyB's node and look for the second output by: 
+```
+run vaultQuery contractStateType: net.corda.samples.multioutput.states.OmniCountState
+```
+Lastly, we would like to show how to introduce new output and update old output at the same transaction. At PartyA's node, run: 
+```
+flow start UpdateOmniCountFromSubCountFlowInitiator receiver: PartyB, amount: 15
+```
+And now if we go to PartyB's node and look for the OmniState, we should see it now shows 30.
+
+
+
+
+
+
+For a guided example of how to extend this template, see the Hello, World! tutorial 
+[here](https://docs.corda.net/hello-world-introduction.html).
---
+++ Features/multioutput-transaction/workflows/src/main/java/net/corda/samples/multioutput/flows/UpdateOmniCountFromSubCountFlow.java
@@ -0,0 +1,118 @@
+package net.corda.samples.multioutput.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.samples.multioutput.contracts.OmniCountContract;
+import net.corda.samples.multioutput.contracts.SubCountContract;
+import net.corda.samples.multioutput.states.OmniCountState;
+import net.corda.samples.multioutput.states.SubCountState;
+import net.corda.core.contracts.StateAndRef;
+import net.corda.core.flows.*;
+import net.corda.core.identity.CordaX500Name;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UpdateOmniCountFromSubCountFlow {
+
+    @InitiatingFlow
+    @StartableByRPC
+    public static class UpdateOmniCountFromSubCountFlowInitiator extends FlowLogic<SignedTransaction> {
+
+        //private variables
+        private Party borrowerMe ;
+        private Party receiver;
+        private Integer amount;
+
+
+        //public constructor
+        public UpdateOmniCountFromSubCountFlowInitiator(Party receiver, Integer amount) {
+
+            this.receiver = receiver;
+            this.amount = amount;
+        }
+
+        @Override
+        @Suspendable
+        public SignedTransaction call() throws FlowException {
+            //Hello World message
+            this.borrowerMe = getOurIdentity();
+
+            // Step 1. Get a reference to the notary service on our network and our key pair.
+            /** Explicit selection of notary by CordaX500Name - argument can by coded in flows or parsed from config (Preferred)*/
+            final Party notary = getServiceHub().getNetworkMapCache().getNotary(CordaX500Name.parse("O=Notary,L=London,C=GB"));
+
+            //Compose the State that carries the Hello World message
+            final SubCountState output = new SubCountState(amount,borrowerMe,receiver);
+
+            StateAndRef inputStateAndRef = getServiceHub().getVaultService().queryBy(OmniCountState.class).getStates().get(0);
+            OmniCountState oldOmni = (OmniCountState) inputStateAndRef.getState().getData();
+
+            final OmniCountState omniCount = new OmniCountState(oldOmni.getOmniAmount()+amount,borrowerMe,receiver,oldOmni.getLinearId());
+
+            // Step 3. Create a new TransactionBuilder object.
+            final TransactionBuilder builder = new TransactionBuilder(notary);
+
+            // Step 4. Add the iou as an output state, as well as a command to the transaction builder.
+            builder.addOutputState(output); //SubCount state
+            builder.addInputState(inputStateAndRef);//Consume the old OmniState
+            builder.addOutputState(omniCount); //new OmniSTate with new balance
+            builder.addCommand(new OmniCountContract.Commands.dualUpdate(),
+                    Arrays.asList(this.borrowerMe.getOwningKey(),this.receiver.getOwningKey()) );
+
+
+            // Step 5. Verify and sign it with our KeyPair.
+            builder.verify(getServiceHub());
+            final SignedTransaction ptx = getServiceHub().signInitialTransaction(builder);
+
+            // Step 6. Collect the other party's signature using the SignTransactionFlow.
+            List<Party> otherParties = output.getParticipants().stream().map(el -> (Party)el).collect(Collectors.toList());
+            otherParties.remove(getOurIdentity());
+            List<FlowSession> sessions = otherParties.stream().map(el -> initiateFlow(el)).collect(Collectors.toList());
+
+            SignedTransaction stx = subFlow(new CollectSignaturesFlow(ptx, sessions));
+
+            // Step 7. Assuming no exceptions, we can now finalise the transaction
+            return subFlow(new FinalityFlow(stx, sessions));
+        }
+    }
+
+    @InitiatedBy(UpdateOmniCountFromSubCountFlowInitiator.class)
+    public static class UpdateOmniCountFromSubCountFlowResponder extends FlowLogic<Void>{
+        //private variable
+        private FlowSession counterpartySession;
+
+        //Constructor
+        public UpdateOmniCountFromSubCountFlowResponder(FlowSession counterpartySession) {
+            this.counterpartySession = counterpartySession;
+        }
+
+        @Suspendable
+        @Override
+        public Void call() throws FlowException {
+            SignedTransaction signedTransaction = subFlow(new SignTransactionFlow(counterpartySession) {
+                @Suspendable
+                @Override
+                protected void checkTransaction(SignedTransaction stx) throws FlowException {
+                    /*
+                     * SignTransactionFlow will automatically verify the transaction and its signatures before signing it.
+                     * However, just because a transaction is contractually valid doesn’t mean we necessarily want to sign.
+                     * What if we don’t want to deal with the counterparty in question, or the value is too high,
+                     * or we’re not happy with the transaction’s structure? checkTransaction
+                     * allows us to define these additional checks. If any of these conditions are not met,
+                     * we will not sign the transaction - even if the transaction and its signatures are contractually valid.
+                     * ----------
+                     * For this hello-world cordapp, we will not implement any aditional checks.
+                     * */
+                }
+            });
+            //Stored the transaction into data base.
+            subFlow(new ReceiveFinalityFlow(counterpartySession, signedTransaction.getId()));
+            return null;
+        }
+    }
+
+}
