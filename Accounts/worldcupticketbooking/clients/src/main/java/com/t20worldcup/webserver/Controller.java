5fcb05226e7db6ff13f0f56bb57042a6288022a0
jakub.zadroga <jakub.zadroga@r3.com>
2024-05-20 18:05:11 +0100

    Upgrade Accounts worldcupticketbooking CorDapp to JDK17 and 4.12-RC01

--- Accounts/worldcupticketbooking/clients/src/main/java/com/t20worldcup/webserver/Controller.java
+++ Accounts/worldcupticketbooking/clients/src/main/java/com/t20worldcup/webserver/Controller.java
@@ -11,7 +11,6 @@ import net.corda.core.node.services.vault.QueryCriteria;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
